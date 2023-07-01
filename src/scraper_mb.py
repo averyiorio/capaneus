54b2217c8f48b74e7aa957f80cded5874cce4b2b
Avery Iorio <24ioria@gmail.com>
2023-07-01 10:31:02 -0400

    scraped moonboard data into csv

--- src/scraper_mb.py
+++ src/scraper_mb.py
@@ -1,54 +1,86 @@
 import requests
+import lxml.html
 from bs4 import BeautifulSoup
+import yaml
+import re
+import json
+import csv
+import os
 
-#def get_problem_names()
+
+def login():
+    """
+    Using account information in the config file to login to moonboard
+    """
+    #getting requests config data
+    with open("config/config.yaml", "r") as f:
+        config = yaml.load(f, Loader=yaml.FullLoader) 
+    
+    s = requests.Session()
+    print(s.cookies.get_dict())
+    login = s.get(config['mb_login_url'])
+    login_html = lxml.html.fromstring(login.text)
+    hidden_inputs = login_html.xpath(r'//form//input[@type="hidden"]')
+    form = {x.attrib["name"]: x.attrib["value"] for x in hidden_inputs}
+
+    dashboard = s.get(config['mb_dashboard_url'])
+    dash_html = lxml.html.fromstring(dashboard.text)
+    hidden_inputs = dash_html.xpath(r'//form//input[@type="hidden"]')
+    form1 = {x.attrib["name"]: x.attrib["value"] for x in hidden_inputs}
+
+    form.update(form1)
+    ### username and password.
+    form['email'] = config['mb_username']
+    form['password'] = config['mb_password']
+    r = s.post(config['mb_login_url'], data=form)
+    return form['__RequestVerificationToken']
 
 
-if __name__ == "__main__":
-    url = "https://www.moonboard.com/Dashboard/GetBenchmarks"
-    headers = {
-    'Accept': '*/*',
-    'Accept-Encoding': 'gzip, deflate, br',
-    'Accept-Language': 'en-US,en;q=0.9',
-    'Connection': 'keep-alive',
-    'Content-Length': '86',
-    'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
-    'Host': 'www.moonboard.com',
-    'Origin': 'https://www.moonboard.com',
-    'Referer': 'https://www.moonboard.com/Dashboard/Index',
-    'sec-ch-ua': '"Chromium";v="112", "Google Chrome";v="112", "Not:A-Brand";v="99"',
-    'sec-ch-ua-mobile': '?0',
-    'sec-ch-ua-platform': '"macOS"',
-    'Sec-Fetch-Dest': 'empty',
-    'Sec-Fetch-Mode': 'cors',
-    'Sec-Fetch-Site': 'same-origin',
-    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36',
-    'X-Requested-With': 'XMLHttpRequest'
-    }
-
+#returns moonboard problem names
+def get_response(auth_token, page_size="10000"):
     payload = {
     'sort': '',
-    'page': '2',
-    'pageSize': '40',
+    'page': '1',
+    'pageSize': page_size,
     'group': '',
     'aggregate': 'Score-sum~MaxScore-sum',
-    'filter': "setupId~eq~'1'"
+    'filter': ""
     }
 
-    response = requests.post(url, data=payload, headers=headers)
+    #getting requests config info
+    with open("config/config.yaml", "r") as f:
+        config = yaml.load(f, Loader=yaml.FullLoader)  
 
-    # Check the response status code
-    if response.status_code == 200:
-        # Print the response content
-        soup = BeautifulSoup(response.content, 'html.parser')
-        # rows = soup.find_all('tr')
-        # for row in rows:
-        #     cells = row.find_all('td')
-        #     print(row)
-        #     for cell in cells:
-        #         print(cell)
+    headers = config['mb_headers']
+    headers['Cookie'] = auth_token + headers['Cookie']
+    response = requests.post(config['mb_bench_url'], data=payload, headers=config['mb_headers'])
 
-        print(soup.prettify())
-
-    else:
+    #check response status code
+    if response.status_code != 200:
         print('Request failed with status code:', response.status_code)
+    return response
+
+
+def save_problem_names(data, filename="../data/mb_problem_names.csv"):
+    """
+    Writes a JSON object to a new csv file
+
+    Args:
+        data (JSON): JSON object with problem names
+    Kwargs:
+        filename (string): filename to write data to (overwrite existing data)
+    """
+    fn = os.path.join(os.path.dirname(__file__), filename)
+    keys = data["Data"][0].keys()  # Get the keys from the first item in the "Data" list
+    with open(fn, mode='w', newline='') as file:
+        writer = csv.DictWriter(file, fieldnames=keys)
+        writer.writeheader()  # Write the header using the keys
+        writer.writerows(data["Data"])  # Write the data rows
+    
+if __name__ == "__main__":
+    #logs in to website with account info provided in the config file
+    token = login()
+    r = get_response(token)
+    soup = BeautifulSoup(r.content, 'html.parser')
+    problem_dict = json.loads(str(soup))
+    save_problem_names(problem_dict)
