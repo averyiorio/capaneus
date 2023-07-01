daa839b917c8f003d836cc43291cfb7d9790e266
Avery Iorio <24ioria@gmail.com>
2023-07-01 12:54:18 -0400

    problem hold scraper

--- src/scraper_mb.py
+++ src/scraper_mb.py
@@ -7,17 +7,16 @@ import json
 import csv
 import os
 
+ #getting requests config info
+with open("config/config.yaml", "r") as f:
+    config = yaml.load(f, Loader=yaml.FullLoader)  
 
 def login():
     """
     Using account information in the config file to login to moonboard
     """
-    #getting requests config data
-    with open("config/config.yaml", "r") as f:
-        config = yaml.load(f, Loader=yaml.FullLoader) 
-    
+
     s = requests.Session()
-    print(s.cookies.get_dict())
     login = s.get(config['mb_login_url'])
     login_html = lxml.html.fromstring(login.text)
     hidden_inputs = login_html.xpath(r'//form//input[@type="hidden"]')
@@ -33,32 +32,49 @@ def login():
     form['email'] = config['mb_username']
     form['password'] = config['mb_password']
     r = s.post(config['mb_login_url'], data=form)
-    return form['__RequestVerificationToken']
+    return s
 
 
 #returns moonboard problem names
-def get_response(auth_token, page_size="10000"):
+def get_problem_names(session, page_size="10000"):
+    # Setup id can be 15, 17, 19
+    # With filter : setupId~eq~'15'~and~Configuration~eq~1
+    # Setup id can be 1
+    # With filter : setupId~eq~'1'
+
     payload = {
     'sort': '',
     'page': '1',
     'pageSize': page_size,
     'group': '',
     'aggregate': 'Score-sum~MaxScore-sum',
-    'filter': ""
+    'filter': "setupId~eq~'1'"
     }
+    board_versions = ["setupId~eq~'17'~and~Configuration~eq~1",
+                      "setupId~eq~'15'~and~Configuration~eq~1",
+                      "setupId~eq~'19'~and~Configuration~eq~1",
+                      "setupId~eq~'17'~and~Configuration~eq~2",
+                      "setupId~eq~'15'~and~Configuration~eq~2",
+                      "setupId~eq~'19'~and~Configuration~eq~2",
+                      "setupId~eq~'1'"]
 
-    #getting requests config info
-    with open("config/config.yaml", "r") as f:
-        config = yaml.load(f, Loader=yaml.FullLoader)  
-
-    headers = config['mb_headers']
-    headers['Cookie'] = auth_token + headers['Cookie']
-    response = requests.post(config['mb_bench_url'], data=payload, headers=config['mb_headers'])
-
-    #check response status code
-    if response.status_code != 200:
-        print('Request failed with status code:', response.status_code)
-    return response
+    # headers = config['mb_headers']
+    # headers['Cookie'] = auth_token + headers['Cookie']
+    problem_names = {}
+    for version in board_versions:
+        payload['filter'] = version
+        print(payload)
+        response = session.post(config['mb_bench_url'], data=payload, headers=config['mb_headers'])
+        #check response status code
+        if response.status_code == 200 and response.content:
+            soup = BeautifulSoup(response.content, 'html.parser')
+            problem_names.update(json.loads(str(soup)))
+            print(len(problem_names))
+        else: 
+            print(f"Unable to find board version {version}")
+    
+    
+    return problem_names
 
 
 def save_problem_names(data, filename="../data/mb_problem_names.csv"):
@@ -76,11 +92,53 @@ def save_problem_names(data, filename="../data/mb_problem_names.csv"):
         writer = csv.DictWriter(file, fieldnames=keys)
         writer.writeheader()  # Write the header using the keys
         writer.writerows(data["Data"])  # Write the data rows
+
+def get_holds(session):
+    problem_names_fn = os.path.join(os.path.dirname(__file__), "../data/mb_problem_names.csv")
+    with open(problem_names_fn, 'r') as file:
+        reader = csv.reader(file)
+        header = next(reader)
+        problem_id = header.index('ProblemId')
+        url_index = header.index('Url')
+        
+        # Read the problem urls
+        urls = []
+        for row in reader:
+            urls.append(row[problem_id] + '/' + row[url_index])
+    
+    problems = []
+    for problem_url in urls:
+        r = session.get("https://www.moonboard.com/Problems/View/" + problem_url, headers=config['mb_problem_headers'])
+        regex_pattern = re.compile(r'(?<=var problem = JSON\.parse\(\').*?(?=\'\);)')
+        problem = re.search(regex_pattern, r.text)
+        problems.append(json.loads(problem.group(0)))
+
+
+
+    problem_holds_fn = os.path.join(os.path.dirname(__file__), "../data/mb_problem_holds.csv")
+    keys = problems[0].keys()  # Get the keys from the first item in the "Data" list
+    with open(problem_holds_fn, mode='w', newline='') as file:
+        writer = csv.DictWriter(file, fieldnames=keys)
+        writer.writeheader()  # Write the header using the keys
+        writer.writerows(problems)  # Write the data rows
+    
+    
     
 if __name__ == "__main__":
-    #logs in to website with account info provided in the config file
-    token = login()
-    r = get_response(token)
-    soup = BeautifulSoup(r.content, 'html.parser')
-    problem_dict = json.loads(str(soup))
-    save_problem_names(problem_dict)
+    #logs in to website with account info provided in the config file\
+    print("Running Scraper")
+    session = login()
+    problem_names = get_problem_names(session)
+    print(problem_names)
+    print(len(problem_names))
+    save_problem_names(problem_names)
+
+    get_holds(session)
+
+    # # Get the variable value
+    # print(script_tag)
+    # print(script_tag.string)
+    # print(script_tag.text)
+
+
+
